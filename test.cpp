b8040b454ab54e41b7bd8729729a763c3b5adf67
Michael Lee <mikecovlee@163.com>
2020-12-03 14:38:48 +0800

    finish

--- test.cpp
+++ test.cpp
@@ -1,10 +1,14 @@
-#include "zip/zip.hpp"
+#include "zip.hpp"
 #include <iostream>
+#include <fstream>
 
 int main()
 {
-    cov::zip file("./covscript.zip");
-    auto buff = file.entry_open("covscript/README.md");
-    std::cout.write((char*)buff.data, buff.size);
+    cov::zip file("./covscript.zip", cov::zip::openmode::append);
+    std::ifstream ifs("./cs.exe", std::ios::binary);
+    if (!ifs)
+        std::cout << "BAD!" << std::endl;
+    else
+        file.write_entry_stream("fxxk/a.exe", ifs);
     return 0;
 }
